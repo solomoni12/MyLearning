a5b1022204471ce3dc427e2f1d72b6577f692d5a
Solomon <mwalupani1234@gmail.com>
2024-02-26 14:48:42 +0300

    email validate

--- filter/filter.php
+++ filter/filter.php
@@ -1,6 +1,6 @@
 <?php
 
-    $str = "<h1>Hello World!</h1>";
+    $str = "<h1> Hello World! </h1>";
     $newstr = filter_var($str, FILTER_SANITIZE_STRING);
     echo $newstr ."<br>";
 
@@ -8,9 +8,21 @@
     $int = 0;
 
     if (filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT) === false) {
-        echo("Integer is valid");
+        echo("Integer is valid" . "<br>");
     } else {
-        echo("Integer is not valid");
+        echo("Integer is not valid". "<br>");
+    }
+
+    $email = "john.doe@example.com";
+
+    // Remove all illegal characters from email
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    // Validate e-mail
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        echo("$email is a valid email address" . "<br>");
+    } else {
+        echo("$email is not a valid email address" . "<br>");
     }
 
 ?>
