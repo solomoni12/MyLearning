138b409372bf46f43ea32f91313e2401e0cb5e55
Peter <nascentmick@gmail.com>
2024-02-26 11:55:16 +0300

    session added

---
+++ form/demo_session.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>session test</title>
+</head>
+<body>
+    <?php 
+        // set the session variables
+        $_SESSION["favcolor"] = "green";
+        $_SESSION["favanimal"] = "cat";
+        echo "Session variables are set.";
+    ?>
+</body>
+</html>
---
+++ form/session.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>session test</title>
+</head>
+<body>
+
+    <?php 
+
+        // display
+
+        echo $_SESSION['favcolor'] . "<br>";
+        echo $_SESSION['favanimal'] . "<br>";
+    ?>
+</body>
+</html>
