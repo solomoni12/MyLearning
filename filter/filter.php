e37dfc5c44af4f03c515ccf7a4712a97d466e6de
Solomon <mwalupani1234@gmail.com>
2024-02-26 14:35:30 +0300

    Update filter.php
    filter

--- filter/filter.php
+++ filter/filter.php
@@ -1,5 +1,15 @@
 <?php
-$str = "<h1>Hello World!</h1>";
-$newstr = filter_var($str, FILTER_SANITIZE_STRING);
-echo $newstr;
+
+    $str = "<h1>Hello World!</h1>";
+    $newstr = filter_var($str, FILTER_SANITIZE_STRING);
+    echo $newstr ."<br>";
+
+
+    $int = "100";
+    if (!filter_var($int, FILTER_VALIDATE_INT) === false) {
+         echo("Integer is valid");
+    } else {
+        echo("Integer is not valid");
+    }
+
 ?>
