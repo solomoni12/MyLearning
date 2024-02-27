92a216fdf9d409eabec35877f1a87e15d0963c68
Solomon <mwalupani1234@gmail.com>
2024-02-27 15:24:00 +0300

    Update array.php

--- form/array.php
+++ form/array.php
@@ -10,16 +10,41 @@
     <?php
 
 
-        $num = [1, 2, 3, 4, 5, 9];
+        $num = [1, 12, 3, 0, 5, 9];
 
         // remove element form an array
-        unset($num["0"]);
+        unset($num["9"]);
 
+
+        // sort num,ber in the array
+        sort($num);
+
+
+        // display number of an array
         echo "Element of an Array is <br>";
         foreach($num as $item){
 
             echo $item;
             echo "<br>";
+        }
+
+
+
+        // car class and their functionality
+        class car{
+            public $color;
+            public $model;
+
+            public function __construct($color,$model){
+                $this->color = $color;
+                $this->model = $model;
+            }
+        }
+
+        $newCar = new Car("red","model");
+
+        foreach($newCar as $car){
+            echo $car ."<br>";
         }
     ?>
 </body>
