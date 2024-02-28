82861676b1137d8a9594724a159f85c00640f24e
Solomon <mwalupani1234@gmail.com>
2024-02-28 15:48:07 +0300

    Update index.php

--- oop/index.php
+++ oop/index.php
@@ -1,18 +1,20 @@
 <?php
 
-include 'Post.php';
-include 'User.php';
-include 'Comment.php';
+    include 'Post.php';
+    include 'User.php';
+    include 'Comment.php';
 
 
-$user = new User("solomon mwalupani");
-$post = new Post('sample post','This sample for post content');
-$comment = new Comment($user, 'Great post');
+    $user = new User("solomon mwalupani");
+    $post = new Post('sample post','This sample for post content');
+    $comment = new Comment($user, 'Great post');
 
 
-echo "post title: " .$post->getTitle() . "<br>";
-echo "post content: " .$post->getContent() . "<br>";
-echo "commented by: " .$comment->getUser()->getUsername() . "<br>";
-echo "comment content: " .$comment->getContent() . "<br>";
+
+    // display content
+    echo "post title: " .$post->getTitle() . "<br>";
+    echo "post content: " .$post->getContent() . "<br>";
+    echo "commented by: " .$comment->getUser()->getUsername() . "<br>";
+    echo "comment content: " .$comment->getContent() . "<br>";
 
 ?>
