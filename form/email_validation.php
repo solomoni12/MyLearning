09de77a6568d82a195b429b62e751d3c6886275d
Solomon <mwalupani1234@gmail.com>
2024-02-29 09:06:03 +0300

    Update email_validation.php

--- form/email_validation.php
+++ form/email_validation.php
@@ -15,9 +15,23 @@
         if (isset($_GET["email"])) {
             if (!filter_input(INPUT_GET, "email", FILTER_VALIDATE_EMAIL) === false) {
                 echo("Email is valid");
+                echo "<br>";
             } else {
                 echo("Email is not valid");
+                echo "<br>";
             }
+        }
+
+
+        // Valiidate integer
+        $num = "42";
+
+        if (filter_var($num, FILTER_VALIDATE_INT) !== false) {
+            echo "$num is a valid integer.";
+            echo "<br>";
+        } else {
+            echo "$num is not a valid integer.";
+            echo "<br>";
         }
     ?>
 
