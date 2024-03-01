cd8a4e1c38cf1d876088375a9ac333a65f6976d8
Solomon <mwalupani1234@gmail.com>
2024-03-01 15:08:45 +0300

    Create sort.php

---
+++ form/sort.php
@@ -0,0 +1,9 @@
+/*
+                        PHP SORTING ARRAY
+    1. sort() - Sort array in ascending alphabetic or numeric order
+    2. rsort() - Sort array in decending alphabetic or numeric order
+    3. asort() - Sort array in ascending in ascenfding order, accoding to value
+    4. ksort() - Sort array in ascending order, accoding to key
+    5. arsort() - Sort array in decending in ascenfding order, accoding to value
+    6. krsort() - Sort array in decending order, accoding to key
+*/
