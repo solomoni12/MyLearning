64bd4ebd2e8e36a1604e667645310171a121dda1
Solomon <mwalupani1234@gmail.com>
2024-03-01 12:52:50 +0300

    Update ajax.php

--- form/ajax.php
+++ form/ajax.php
@@ -9,9 +9,9 @@
                 } else {
                     var xmlhttp = new XMLHttpRequest();
                     xmlhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("txtHint").innerHTML = this.responseText;
-                    }
+                        if (this.readyState == 4 && this.status == 200) {
+                            document.getElementById("txtHint").innerHTML = this.responseText;
+                        }
                     }
                     xmlhttp.open("GET", "gethint.php?q="+str, true);
                     xmlhttp.send();
@@ -23,8 +23,8 @@
 
         <p><b>Start typing a name in the input field below:</b></p>
         <form action="">
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
         </form>
         <p>Suggestions: <span id="txtHint"></span></p>
 
