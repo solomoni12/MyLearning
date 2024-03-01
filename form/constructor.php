e09e39280e5c1711da32a1d15747ca069fbc3357
Solomon <mwalupani1234@gmail.com>
2024-03-01 16:52:07 +0300

    Update constructor.php

--- form/constructor.php
+++ form/constructor.php
@@ -7,14 +7,30 @@
                 public $name;
                 public $color;
 
-                function __construct($name) {
+                function __construct($name, $color) {
                     $this->name = $name; 
+                    $this->color = $color;
                 }
+
+                public function getName(){
+                    return $this->name;
+                }
+
+                public function getColor(){
+                    return $this->color;
+                }
+
                 function __destruct() {
-                    echo "The fruit is {$this->name}."; 
+                    echo "The fruit name is {$this->name} and color is {$this->color}"; 
+                    echo "<br>";
                 }
             }
-            $apple = new Fruit("Apple");
+            $apple = new Fruit("Apple", "Red");
+
+            echo $apple->getName() . "<br>";
+            echo $apple->getColor() . "<br>";
+
+            
         ?>
     
     </body>
