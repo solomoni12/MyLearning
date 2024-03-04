091d01cbbdcf2ccf91c963e7da21e47dee2601c5
Solomon <mwalupani1234@gmail.com>
2024-03-04 15:52:58 +0300

    cancel and success php added

---
+++ form/paypal/cancel.php
@@ -0,0 +1,4 @@
+<?php
+// Display a message to the user
+    echo "Payment was canceled. If you have any questions, please contact support.";
+?>
---
+++ form/paypal/success.php
@@ -0,0 +1,6 @@
+<?php
+    // Display a thank you message to the user
+    echo "Thank you for your payment. Your order has been successfully processed.";
+
+    // Additional logic: Update your database, send confirmation emails, etc.
+?>
