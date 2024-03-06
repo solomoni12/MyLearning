6a7f4104c86bc7fe6e73b8dbef85fb98fbd6ad3a
Solomon <mwalupani1234@gmail.com>
2024-03-06 10:04:15 +0300

    Create convert.php

---
+++ form/convert.php
@@ -0,0 +1,19 @@
+<?php 
+    echo strtoupper("hello mwalupani") . "<br>";
+    echo strtolower("HELLO MWALUPANI") . "<br>";
+    echo lcfirst("HELLO MWALUPANI") . "<br>";
+    echo ucfirst("hello mwalupani") . "<br>";
+    echo ucwords("hello mwalupani") . "<br>";
+    // echo lcwords("HELLO MWALUPANI") . "<br>";
+
+    function ucwordsLowerFirst($inputString) {
+        $words = explode(' ', $inputString);
+        $lowercasedWords = array_map('lcfirst', $words);
+        $resultString = implode(' ', $lowercasedWords);
+        return $resultString;
+    }
+    
+    echo ucwordsLowerFirst("HELLO MWALUPANI") . "<br>";
+    
+
+?>
