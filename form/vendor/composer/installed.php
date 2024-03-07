b15e6bc0b8d519ddd69c69f84496849fb75a9699
Solomon <mwalupani1234@gmail.com>
2024-03-07 09:11:29 +0300

    php mailer

---
+++ form/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dev/form',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cc13011e08f2a5a99df6a0f195d3ab084d94af87',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev/form' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cc13011e08f2a5a99df6a0f195d3ab084d94af87',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
