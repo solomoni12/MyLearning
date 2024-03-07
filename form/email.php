aa5eefa7b41e19b8f14d30964f8c7a3ab5a11c34
Solomon <mwalupani1234@gmail.com>
2024-03-07 15:58:55 +0300

    Update email.php

--- form/email.php
+++ form/email.php
@@ -1,86 +1,82 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+    require 'C:\xampp\htdocs\MyLearning\form\PHPMailer\src\PHPMailer.php';
+    require 'C:\xampp\htdocs\MyLearning\form\PHPMailer\src\Exception.php';
+    require 'C:\xampp\htdocs\MyLearning\form\PHPMailer\src\SMTP.php';
 
-require 'C:\xampp\htdocs\MyLearning\form\PHPMailer\src\PHPMailer.php';
-require 'C:\xampp\htdocs\MyLearning\form\PHPMailer\src\Exception.php';
-require 'C:\xampp\htdocs\MyLearning\form\PHPMailer\src\SMTP.php';
+    $errors = [];
+    $errorMessage = '';
+    $successMessage = '';
 
-$errors = [];
-$errorMessage = '';
-$successMessage = '';
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = sanitizeInput($_POST['name']);
+        
+        // sanitize email 
+        $emil = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    $name = sanitizeInput($_POST['name']);
-    
-    // sanitize email 
-    $emil = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $email = sanitizeInput($emil);
+        $message = sanitizeInput($_POST['message']);
 
-    $email = sanitizeInput($emil);
-    $message = sanitizeInput($_POST['message']);
+        if (empty($name)) {
+            $errors[] = 'Name is empty';
+        }
 
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
+        if (empty($email)) {
+            $errors[] = 'Email is empty';
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Email is invalid';
+        }
 
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
+        if (empty($message)) {
+            $errors[] = 'Message is empty';
+        }
 
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
+        if (!empty($errors)) {
+            $allErrors = join('<br/>', $errors);
+            $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+        } else {
+            $toEmail = 'mailtrap.club@gmail.com';
+            $emailSubject = $name;
 
-    if (!empty($errors)) {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    } else {
-        $toEmail = 'mailtrap.club@gmail.com';
-        $emailSubject = 'New email from your contact form';
+            // Create a new PHPMailer instance
+            $mail = new PHPMailer(true);
 
-        // Create a new PHPMailer instance
-        $mail = new PHPMailer(true);
+            try {
+                // Configure the PHPMailer instance
+                $mail->isSMTP();
+                $mail->Host = 'sandbox.smtp.mailtrap.io';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'e077253dcde2d0';
+                $mail->Password = 'a8f3406b7e305a';
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587;
 
-        try {
-            // Configure the PHPMailer instance
-            $mail->isSMTP();
-            $mail->Host = 'sandbox.smtp.mailtrap.io';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'e077253dcde2d0';
-            $mail->Password = 'a8f3406b7e305a';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port = 587;
+                // Set the sender, recipient, subject, and body of the message
+                $mail->setFrom($email);
+                $mail->addAddress($toEmail);
+                $mail->Subject = $emailSubject;
+                $mail->isHTML(true);
+                $mail->Body = "<p>Name: {$name}</p><p>Email: {$email}</p><p>Message: {$message}</p>";
 
-            // Set the sender, recipient, subject, and body of the message
-            $mail->setFrom($email);
-            $mail->addAddress($toEmail);
-            $mail->Subject = $emailSubject;
-            $mail->isHTML(true);
-            $mail->Body = "<p>Name: {$name}</p><p>Email: {$email}</p><p>Message: {$message}</p>";
+                $mail->send();
 
-            $mail->send();
-            // echo "<pre>";
-            // print_r($mail);
-            // exit();
-
-            $successMessage = "<p style='color: green;'>Thank you for contacting us :</p>";
-        } catch (Exception $e) {
-            $errorMessage = "<p style='color: red;'>Oops, something went wrong. Please try again later</p>";
+                $successMessage = "<p style='color: green;'>Thank you for contacting us! Your message has been successfully received. We appreciate your inquiry and will get back to you as soon as possible. If your matter is urgent, please call us at 0789026656.</p>";
+              } catch (Exception $e) {
+                $errorMessage = "<p style='color: red;'>Oops, something went wrong. Please try again later</p>";
+            }
         }
     }
-}
 
-function sanitizeInput($input)
-{
-    $input = trim($input);
-    $input = stripslashes($input);
-    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-    return $input;
-}
+    function sanitizeInput($input)
+    {
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        return $input;
+    }
 
 ?>
 
