d6e5c50487f662b950aa08017dbff5a3aa2169ae
Solomon <mwalupani1234@gmail.com>
2024-03-09 11:26:13 +0300

    Update calculator.php

--- form/calculator.php
+++ form/calculator.php
@@ -4,24 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator test</title>
+    <style>
+        .card{
+            margin-top: 20px;
+            margin-bottom: 10px;
+            margin-left: 100px;
+            margin-right: 100px;
+        }
+        .card-form{
+            margin-top: 20px;
+            margin-bottom: 10px;
+            margin-left: 300px;
+            margin-right: 300px;
+        }
+        .select-field{
+            margin-top: 20px;
+            margin-bottom: 10px;
+            margin-left: 300px;
+            margin-right: 300px;
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
 </head>
 <body>
     
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+    <div class="card">
+        <form class="card-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
 
-        <input type="text" name="num01" placeholder="Number one" required>
-        
-        <select name="operator" id="operator" required>
-            <option value="add">+</option>
-            <option value="sub">-</option>
-            <option value="mult">*</option>
-            <option value="divide">/</option>
-        </select>
+           <div class="form-group">
+            <input class="form-control" type="text" name="num01" placeholder="Number one" required>
+           </div>
 
-        <input type="text" name="num02" placeholder="Number two" required>
+           <div class="form-group select-field">
+            <select class="form-control" name="operator" id="operator" required>
+                    <option value="add">+</option>
+                    <option value="sub">-</option>
+                    <option value="mult">*</option>
+                    <option value="divide">/</option>
+                </select>
+           </div>
 
-        <button>calculate</button>
-    </form>
+            <div class="form-group">
+                <input class="form-control" type="text" name="num02" placeholder="Number two" required>
+            </div>
+
+            <button class="btn btn-primary" >calculate</button>
+        </form>
+
+    </div>
 
     <?php
 
@@ -32,14 +64,24 @@
             $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
             $operator = htmlspecialchars($_POST["operator"]);
 
+            // echo "<pre>";
+            // print_r($num01);
+            // echo "<pre>";
+            // print_r($num02);
+            // echo "<pre>";
+            // print_r($operator);
+            // exit();
+
             $error = false;
 
-            if (!($num01 == 0 || $num02 == 0) || empty($operator)) {
-                echo "num01 and num02 should not be 0, and operator is required";
+            if (empty($num01) || (!is_numeric($num02) && $num02 != 0) || empty($operator)) {
+                echo "num01, num02, and operator are required";
+                $error = true;
+            } elseif ($num02 == 0 && $operator == '/') {
+                echo "Cannot divide by zero";
                 $error = true;
             }
             
-
             if(!is_numeric($num01) || !is_numeric($num02)){
 
                 echo "all number are required";
@@ -78,7 +120,6 @@
                 }
 
                 echo $value;
-                // var_dump($value);
             }
 
         }
