8b94ea98b15c7fce9ed405071cfa9628c2a61e8d
Solomon <mwalupani1234@gmail.com>
2024-03-11 09:49:49 +0300

    Update form_validation.php

--- form/form_validation.php
+++ form/form_validation.php
@@ -2,104 +2,164 @@
 <html>
   <head>
     <style>
-      .error {color: #FF0000;}
+      .error {
+        color: #FF0000;
+      }
+      .card {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        margin-left: 200px;
+        margin-right: 200px;
+      }
+      .card-form {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        margin-left: 200px;
+        margin-right: 200px;
+      }
+      .card-output {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        margin-left: 200px;
+        margin-right: 200px;
+      }
+      .title{
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: large;
+        text-align: center;
+      }
     </style>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
   </head>
   <body>  
 
-      <?php
-          // define variables and set to empty values
-          $nameErr = $emailErr = $genderErr = $websiteErr = "";
-          $name = $email = $gender = $comment = $website = "";
+    <?php
+    // define variables and set to empty values
+      $nameErr = $emailErr = $genderErr = $websiteErr = "";
+      $name = $email = $gender = $comment = $website = "";
 
-          if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST["name"])) {
-              $nameErr = "Name is required";
-            } else {
-              $name = test_input($_POST["name"]);
-              // check if name only contains letters and whitespace
-              if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-                $nameErr = "Only letters and white space allowed";
-              }
-            }
-            
-            if (empty($_POST["email"])) {
-              $emailErr = "Email is required";
-            } else {
-              $email = test_input($_POST["email"]);
-              // check if e-mail address is well-formed
-              if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
-              }
-            }
-              
-            if (empty($_POST["website"])) {
-              $website = "";
-            } else {
-              $website = test_input($_POST["website"]);
-              // check if URL address syntax is valid
-              if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-                $websiteErr = "Invalid URL";
-              }    
-            }
-
-
-            if (empty($_POST["comment"])) {
-              $comment = "";
-            } else {
-              $comment = test_input($_POST["comment"]);
-            }
-
-            if (empty($_POST["gender"])) {
-              $genderErr = "Gender is required";
-            } else {
-              $gender = test_input($_POST["gender"]);
-            }
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+          $nameErr = "Name is required";
+        } else {
+          $name = test_input($_POST["name"]);
+          // check if name only contains letters and whitespace
+          if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
           }
+        }
 
-          function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
+        if (empty($_POST["email"])) {
+          $emailErr = "Email is required";
+        } else {
+          $email = test_input($_POST["email"]);
+          // check if e-mail address is well-formed
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
           }
-      ?>
+        }
 
-      <h2>PHP Form Validation Example</h2>
-      <p><span class="error">* required field</span></p>
-      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-          Name: <input type="text" name="name">
-          <span class="error">* <?php echo $nameErr;?></span>
-          <br><br>
-          E-mail: <input type="text" name="email">
-          <span class="error">* <?php echo $emailErr;?></span>
-          <br><br>
-          Website: <input type="text" name="website">
+        if (empty($_POST["website"])) {
+          $website = "";
+        } else {
+          $website = test_input($_POST["website"]);
+          // check if URL address syntax is valid
+          if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
+            $websiteErr = "Invalid URL";
+          }    
+        }
+
+        if (empty($_POST["comment"])) {
+          $comment = "";
+        } else {
+          $comment = test_input($_POST["comment"]);
+        }
+
+        if (empty($_POST["gender"])) {
+          $genderErr = "Gender is required";
+        } else {
+          $gender = test_input($_POST["gender"]);
+        }
+      }
+
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
+
+    <div class="card">
+      <form class="card-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+        <h2 class="title">PHP Form Validation Example</h2>
+        <div class="form-group">
+          <label for="Name">Name<span class="error">*</span></label> 
+          <input type="text" name="name" class="form-control">
+          <span class="error"> <?php echo $nameErr;?></span>
+        </div>
+        <div class="form-group">
+          <label for="E-mail">E-mail<span class="error">*</span></label>
+          <input type="text" name="email" class="form-control">
+          <span class="error"> <?php echo $emailErr;?></span>
+        </div>
+        <div class="form-group">
+          <label for="Gender">Gender<span class="error">*</span></label>
+          <select name="gender" id="gender" class="form-control">
+            <option value="male" >male</option>
+            <option value="female">female</option>
+            <option value="other" >other</option>
+          </select>
+          <span class="error"><?php echo $genderErr;?></span>
+        </div>
+        <div class="form-group">
+          <label for=" Website"> Website: </label>
+          <input type="text" name="website" class="form-control" >
           <span class="error"><?php echo $websiteErr;?></span>
+        </div>
+        <div class="form-group">
+          <label for="Comment">Comment:</label> 
+          <textarea name="comment" rows="5" cols="40" class="form-control"></textarea>
           <br><br>
-          Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-          <br><br>
-          Gender:
-          <input type="radio" name="gender" value="female">Female
-          <input type="radio" name="gender" value="male">Male
-          <input type="radio" name="gender" value="other">Other
-          <span class="error">* <?php echo $genderErr;?></span>
-          <br><br>
-          <input type="submit" name="submit" value="Submit">  
+        </div>
+        <input type="submit" name="submit" class="btn btn-primary" value="Submit">  
+
       </form>
 
-      <?php
-        echo "<h2>Your Input:</h2>";
-        echo $name;
-        echo "<br>";
-        echo $email;
-        echo "<br>";
-        echo $website;
-        echo "<br>";
-        echo $comment;
-        echo "<br>";
-        echo $gender;
-      ?>
+      <div class="card-output">
 
+        <?php
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            echo "<h2 class='title' >Your Input:</h2>";
+            echo "<table class='table table-bordered'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>S/N</th>";
+            echo "<th>Name</th>";
+            echo "<th>Email</th>";
+            echo "<th>Gender</th>";
+            echo "<th>Website</th>";
+            echo "<th>Comment</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            echo "<tr>";
+            echo "<td>1</td>";
+            echo "<td>$name</td>";
+            echo "<td>$email</td>";
+            echo "<td>$gender</td>";
+            echo "<td>$website</td>";
+            echo "<td>$comment</td>";
+            echo "</tr>";
+            echo "</tbody>";
+            echo "</table>";
+          }
+        ?>
+
+      </div>
+    </div>
   </body>
 </html>
