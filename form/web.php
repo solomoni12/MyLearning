72735cef4f0f53b010964c930d8ad865d75748f5
Solomon <mwalupani1234@gmail.com>
2024-03-12 11:47:01 +0300

    image and web update

--- form/web.php
+++ form/web.php
@@ -6,37 +6,192 @@
     <title>Websites</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+        <style>
+        /* .container{
+            background-color: red;
+        } */
+    </style>
 </head>
 <body>
     <div class="m-4">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a href="#" class="navbar-brand">Demo</a>
+                <a href="#home" class="navbar-brand">Demo</a>
                 <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto">
                         <a href="#home" class="nav-item nav-link active">Home</a>
-                        <a href="#profile" class="nav-item nav-link">Profile</a>
-                        <a href="#message" class="nav-item nav-link">Messages</a>
-                        <a href="#reports" class="nav-item nav-link disabled" tabindex="-1">Reports</a>
+                        <a href="#about" class="nav-item nav-link">About Us</a>
+                        <a href="#service" class="nav-item nav-link">Service</a>
+                        <a href="#team" class="nav-item nav-link">Our Team</a>
+                        <a href="#contact" class="nav-item nav-link">Contact Us</a>
                     </div>
                 </div>
             </div>
         </nav>
     </div>
-    <div id="home">
-      
+    <div class="container">
+        <div id="home" class="row p-5 my-4 bg-light">
+            <div class="col-md-8">
+                <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Solomon Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Full Stack Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <img src="../form/img/avatar.png" class="img-fluid" alt="Cover Image">
+            </div>
+        </div>
+        <div id="about" class="row p-5 my-4 bg-light">
+            <div class="col-md-8">
+                <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Solomon Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Full Stack Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <img src="../form/img/avatar.png" class="img-fluid" alt="Cover Image">
+            </div>
+        </div>
+        <div id="service" class="row p-5 my-4 bg-light">
+            <div class="col-md-8">
+                <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Solomon Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Full Stack Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <img src="../form/img/avatar.png" class="img-fluid" alt="Cover Image">
+            </div>
+        </div>
+        <div id="team" class="row p-5 my-4 bg-light">
+            <div class="col-md-8">
+                <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Solomon Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div>
+                                <h1>HELLO I AM </h1>
+                                <p>Full Stack Developer</p>
+                                <h6>welcome to my site, change the word with solomon developer.</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <img src="../form/img/avatar.png" class="img-fluid" alt="Cover Image">
+            </div>
+        </div>
+        <div id="contact" class="row p-5 my-4 bg-light">
+            <div class="m-4">
+                <form action="/examples/actions/confirmation.php" method="post">
+                    <div class="row mb-3">
+                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="inputPassword" placeholder="Password" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-10 offset-sm-2">
+                            <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="checkRemember">
+                                    <label class="form-check-label" for="checkRemember">Remember me</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-10 offset-sm-2">
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <footer>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>Copyright © 2024 Demo</p>
+                    </div>
+                    <div class="col-md-6 text-md-end">
+                        <a href="#" class="text-dark">Terms of Use</a>
+                        <span class="text-muted mx-2">|</span>
+                        <a href="#" class="text-dark">Privacy Policy</a>
+                    </div>
+                </div>
+            </footer>
     </div>
-    <div id="profile">
-        hello
-    </div>
-    <div id="message">
-    
-    </div>
-    <div id="reports">
-        reort
-    </div>
+
 </body>
 </html>
