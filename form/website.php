736b873c07b5f302fb605b99a267f87a49b595e4
Solomon <mwalupani1234@gmail.com>
2024-03-12 09:08:31 +0300

    Create website.php

---
+++ form/website.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website test</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <class class="container-lg">
+        <div class="row">
+            <div class="col-xl-4">box1</div>
+            <div class="col-xl-4">box2</div>
+            <div class="col-xl-4">box3</div>
+            <div class="col-xl-4">box4</div>
+            <div class="col-xl-4">box5</div>
+            <div class="col-xl-4">box6</div>
+            <div class="col-xl-4">box7</div>
+            <div class="col-xl-4">box8</div>
+            <div class="col-xl-4">box9</div>
+            <div class="col-xl-4">box10</div>
+            <div class="col-xl-4">box11</div>
+            <div class="col-xl-4">box12</div>
+        </div>
+    </class>
+</body>
+</html>
