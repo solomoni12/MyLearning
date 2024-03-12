14d3b74d5061b695734865e5da2fd3f5216995ef
Solomon <mwalupani1234@gmail.com>
2024-03-12 10:05:02 +0300

    Create web.php

---
+++ form/web.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Websites</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="m-4">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand">Demo</a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto">
+                        <a href="#home" class="nav-item nav-link active">Home</a>
+                        <a href="#profile" class="nav-item nav-link">Profile</a>
+                        <a href="#message" class="nav-item nav-link">Messages</a>
+                        <a href="#reports" class="nav-item nav-link disabled" tabindex="-1">Reports</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div id="home">
+      
+    </div>
+    <div id="profile">
+        hello
+    </div>
+    <div id="message">
+    
+    </div>
+    <div id="reports">
+        reort
+    </div>
+</body>
+</html>
