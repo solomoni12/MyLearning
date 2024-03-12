8b32d5badce29606fc82b7e21951ea774022b727
Solomon <mwalupani1234@gmail.com>
2024-03-12 09:29:59 +0300

    Update website.php

--- form/website.php
+++ form/website.php
@@ -7,23 +7,60 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <style>
+    /* Some custom styles to beautify this example */
+    .box{
+        padding: 50px;
+        font-size: 32px;
+        font-weight: bold;
+        text-align: center;
+        background: #dbdfe5;
+    }
+</style>
 </head>
 <body>
-    <class class="container-lg">
+    <h2 class="text-center mt-3"></h2>
+	<div class="text-center my-3"></div>
+    <div class="container-lg">
         <div class="row">
-            <div class="col-xl-4">box1</div>
-            <div class="col-xl-4">box2</div>
-            <div class="col-xl-4">box3</div>
-            <div class="col-xl-4">box4</div>
-            <div class="col-xl-4">box5</div>
-            <div class="col-xl-4">box6</div>
-            <div class="col-xl-4">box7</div>
-            <div class="col-xl-4">box8</div>
-            <div class="col-xl-4">box9</div>
-            <div class="col-xl-4">box10</div>
-            <div class="col-xl-4">box11</div>
-            <div class="col-xl-4">box12</div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box1</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box2</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box3</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box4</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box5</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box6</p>>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box7</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+              <p class="box">box8</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+              <p class="box">box9</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+              <p class="box">box10</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box12</p>
+            </div>
+            <div class="col-xl-4 col-md-6 col-xxl-3">
+                <p class="box">box12</p>
+            </div>
         </div>
-    </class>
+    </div>
 </body>
 </html>
