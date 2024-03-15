e5590227217c0592adfa3252eeee9ef20e84f820
Solomon <mwalupani1234@gmail.com>
2024-03-15 14:12:26 +0300

    update

---
+++ form/backup-color_dira_backend.php
@@ -0,0 +1,3 @@
+<!--        style="background: linear-gradient(to right, #030000, rgba(3, 3, 3, 0.95), #00AEEF); color: white;"-->
+<!--   style="background: -webkit-linear-gradient(315deg, hsla(212, 100%, 27%, 1) 0%, hsla(115, 52%, 29%, 1) 100%); background: linear-gradient(to right, #030000, rgba(3, 3, 3, 0.95), #00AEEF); color: white;"     -->
+<!--        style="background-color: silver; color: black;"-->
---
+++ form/laravel_baqckend.php
@@ -0,0 +1,150 @@
+1. https://mvs.bslmeiyu.com/api/v1/products/recommended
+{
+  "total_size": 5,
+  "type_id": 3,
+  "offset": 0,
+  "products": [
+    {
+      "id": 3,
+      "name": "Chinese Side",
+      "description": "Enjoy these flavors any time of day.",
+      "price": 34,
+      "stars": 3,
+      "img": "images/fe978d4b2a6e311efc53370bad41c302.jpg",
+      "location": "Moraine Lake, Alberta",
+      "created_at": "2021-11-17 10:16:31",
+      "updated_at": "2022-05-16 11:49:24",
+      "type_id": 3
+    },
+    {
+      "id": 4,
+      "name": "Bitter Orange Marinade",
+      "description": "Bitter oranges are often used in Caribbean ",
+      "price": 33,
+      "stars": 4,
+      "img": "images/f38f470cc1972c270320c222c3aca9fb.jpg",
+      "location": "Killarney Provincial Park",
+      "created_at": "2021-11-17 10:18:46",
+      "updated_at": "2022-01-01 08:55:05",
+      "type_id": 3
+    },
+    {
+      "id": 5,
+      "name": "Creamed Yuca (Cassava)",
+      "description": "Commonly used in soups, eaten boiled ",
+      "price": 10,
+      "stars": 5,
+      "img": "images/acaa4cdee4b8aa7bf33f2140ce36860c.jpg",
+      "location": "China",
+      "created_at": "2021-12-08 12:29:53",
+      "updated_at": "2022-01-01 08:55:44",
+      "type_id": 3
+    },
+    {
+      "id": 8,
+      "name": "Dessert",
+      "description": "Amazing food to eat with.",
+      "price": 5,
+      "stars": 4,
+      "img": "images/34b0eaad01e8a95a02df1d934517591c.png",
+      "location": "china",
+      "created_at": "2021-12-26 01:36:29",
+      "updated_at": "2022-01-01 08:57:01",
+      "type_id": 3
+    },
+    {
+      "id": 9,
+      "name": "Biriani",
+      "description": "Chicken marinated in a spiced yoghurt is placed in a large pot, then layered with fried onions (cheeky easy sub below!), fresh coriander/cilantro, then par boiled lightly spiced rice.The crowning glory is to finish it off with a drizzle of saffron infused water to give it the signature patches of bright yellow rice, as well as ghee (or melted butter) for buttery richness.The pot is then covered and cooked over a low heat for about 25 minutes during which time the rice absorbs the aromas and flavours of the curry underneath, whilst still being beautifully fluffy.",
+      "price": 8,
+      "stars": 4,
+      "img": "images/0f61cb5dd19d38a11e5a9133333eca07.jpeg",
+      "location": "BD",
+      "created_at": "2021-12-27 06:27:38",
+      "updated_at": "2022-01-01 08:57:16",
+      "type_id": 3
+    }
+  ]
+}
+
+2. https://mvs.bslmeiyu.com/api/v1/products/popular
+
+{
+  "total_size": 6,
+  "type_id": 2,
+  "offset": 0,
+  "products": [
+    {
+      "id": 1,
+      "name": "Easy Saucy Ramen Noodles",
+      "description": "This five red bases, two are dedicated to salami (fennel and regular), and there s a classic capricciosa or beef carpaccio. Blanco options include a mushroom, and a four-cheese extravaganza featuring great lobes of a tangy fior di latte they make in house every day (more on this later). Classic, precise, good.I think it s still better to think of this venue not as a pizzeria, but as Pizza, by Di Stasio.Rinaldo Di Stasio and Mallory Wall s empire, including the original restaurant and bar in St Kilda, Citta and now Carlton, is a designer label for dining.",
+      "price": 120,
+      "stars": 4,
+      "img": "images/0c264eed2ae5dc14aa916036568270a8.jpg",
+      "location": "Canada, British Columbia",
+      "created_at": "2021-11-17 10:09:08",
+      "updated_at": "2023-11-08 10:33:48",
+      "type_id": 2
+    },
+    {
+      "id": 2,
+      "name": "Muschel-Spaghetti",
+      "description": "The best sweet dessertThe quality and craftsmanship is on full show. The waiters  signature white tunics are of loosely rumpled linen here versus the crisp whites of the city. The thick galleria walls of the restaurant s three distinct areas are cast in gentle neon so that even your worst wine-fuelled photos turn into atmospheric still lifes. The art, typically, lands a gut punch.From the Bill Henson portraits at the St Kilda restaurant to Shaun Gladwell s beautiful video artworks at Citta, Di Stasio doesn t use pieces to decorate but to provoke. On entry, a bronze cast of Di Stasio s hands jutt out of the wall, immortalised in the sign of the bull – equal parts a rock star welcome and a warning. A trio of powerful works by Kamilaroi artist Reko Rennie, titled Genesis, goes straight for the jugular of unceded sovereignty.The quality and craftsmanship is on full show. The waiters  signature white tunics are of loosely rumpled linen here versus the crisp whites of the city. The thick galleria walls of the restaurant s three distinct areas are cast in gentle neon so that even your worst wine-fuelled photos turn into atmospheric still lifes. The art, typically, lands a gut punch.From the Bill Henson portraits at the St Kilda restaurant to Shaun Gladwell s beautiful video artworks at Citta, Di Stasio doesn t use pieces to decorate but to provoke. On entry, a bronze cast of Di Stasio s hands jutt out of the wall, immortalised in the sign of the bull – equal parts a rock star welcome and a warning. A trio of powerful works by Kamilaroi artist Reko Rennie, titled Genesis, goes straight for the jugular of unceded sovereignty.The quality and craftsmanship is on full show. The waiters  signature white tunics are of loosely rumpled linen here versus the crisp whites of the city. The thick galleria walls of the restaurant s three distinct areas are cast in gentle neon so that even your worst wine-fuelled photos turn into atmospheric still lifes. The art, typically, lands a gut punch.From the Bill Henson portraits at the St Kilda restaurant to Shaun Gladwell s beautiful video artworks at Citta, Di Stasio doesn t use pieces to decorate but to provoke. On entry, a bronze cast of Di Stasio s hands jutt out of the wall, immortalised in the sign of the bull – equal parts a rock star welcome and a warning. A trio of powerful works by Kamilaroi artist Reko Rennie, titled Genesis, goes straight for the jugular of unceded sovereignty.",
+      "price": 21,
+      "stars": 5,
+      "img": "images/5dfa53f0783af1e631ae6d7f8379e2b5.jpg",
+      "location": "Icefields, Alberta",
+      "created_at": "2021-11-17 10:10:43",
+      "updated_at": "2023-11-08 10:34:46",
+      "type_id": 2
+    },
+    {
+      "id": 6,
+      "name": "No-Yeast Pissa Dough",
+      "description": "The quality and craftsmanship is on full show. The waiters  signature white tunics are of loosely rumpled linen here versus the crisp whites of the city. The thick galleria walls of the restaurant s three distinct areas are cast in gentle neon so that even your worst wine-fuelled photos turn into atmospheric still lifes. The art, typically, lands a gut punch.From the Bill Henson portraits at the St Kilda restaurant to Shaun Gladwell s beautiful video artworks at Citta, Di Stasio doesn t use pieces to decorate but to provoke. On entry, a bronze cast of Di Stasio s hands jutt out of the wall, immortalised in the sign of the bull – equal parts a rock star welcome and a warning. A trio of powerful works by Kamilaroi artist Reko Rennie, titled Genesis, goes straight for the jugular of unceded sovereignty.",
+      "price": 18,
+      "stars": 4,
+      "img": "images/1d482dfa006b8dd4eaefe9f48775a4b2.jpg",
+      "location": "china",
+      "created_at": "2021-12-23 15:03:24",
+      "updated_at": "2023-11-08 10:35:36",
+      "type_id": 2
+    },
+    {
+      "id": 7,
+      "name": "Skinny Chicken and Roasted Potato Bowl",
+      "description": "The quality and craftsmanship is on full show. The waiters  signature white tunics are of loosely rumpled linen here versus the crisp whites of the city. The thick galleria walls of the restaurant s three distinct areas are cast in gentle neon so that even your worst wine-fuelled photos turn into atmospheric still lifes. The art, typically, lands a gut punch.From the Bill Henson portraits at the St Kilda restaurant to Shaun Gladwell s beautiful video artworks at Citta, Di Stasio doesn t use pieces to decorate but to provoke. On entry, a bronze cast of Di Stasio s hands jutt out of the wall, immortalised in the sign of the bull – equal parts a rock star welcome and a warning. A trio of powerful works by Kamilaroi artist Reko Rennie, titled Genesis, goes straight for the jugular of unceded sovereignty.",
+      "price": 10,
+      "stars": 5,
+      "img": "images/9a27af7a8ac9251757365fa19970ab99.jpg",
+      "location": "Cuba",
+      "created_at": "2021-12-23 15:04:49",
+      "updated_at": "2023-11-08 10:36:26",
+      "type_id": 2
+    },
+    {
+      "id": 10,
+      "name": "Pasta with Vegetables",
+      "description": "Chicken Biryani Served in a Bowl Close Up Photo. Indian Food, Indian Style Rice and Chicken. #Biryani #ChickenBiryani #IndianFood #StockPhoto",
+      "price": 12,
+      "stars": 4,
+      "img": "images/0051e5876c365ebc0385e52e11718b42.jpg",
+      "location": "Morocco",
+      "created_at": "2021-12-27 06:29:47",
+      "updated_at": "2023-11-08 10:31:21",
+      "type_id": 2
+    },
+    {
+      "id": 11,
+      "name": "Fruit",
+      "description": "Ilish Mach aka Hilsa fish is in the season and needless to say, I cooked it a few times this year as well. This year, however, I have used Ilish mach to make simple delicacies mostly. Ilish Beguner Jhol is probably the simplest of the lot, well, after Ilish Mach Bhaja.Ilish Mach aka Hilsa fish is in the season and needless to say, I cooked it a few times this year as well. This year, however, I have used Ilish mach to make simple delicacies mostly. Ilish Beguner Jhol is probably the simplest of the lot, well, after Ilish Mach Bhaja.Ilish Mach aka Hilsa fish is in the season and needless to say, I cooked it a few times this year as well. This year, however, I have used Ilish mach to make simple delicacies mostly. Ilish Beguner Jhol is probably the simplest of the lot, well, after Ilish Mach Bhaja.",
+      "price": 150,
+      "stars": 5,
+      "img": "images/bb318659e42af2acabe5a4db0f83c72b.jpg",
+      "location": "Morocco",
+      "created_at": "2021-12-27 06:35:34",
+      "updated_at": "2023-11-08 10:32:33",
+      "type_id": 2
+    }
+  ]
+}
